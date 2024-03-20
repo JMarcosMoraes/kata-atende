2b3490043037582acdbcff6a1643d9b4743702a0
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-03-19 21:18:27 -0300

    Classes Domain

--- src/main/java/com/kata/helpDesk/domain/enums/Status.java
+++ src/main/java/com/kata/helpDesk/domain/enums/Status.java
@@ -1,29 +1,33 @@
 package com.kata.helpDesk.domain.enums;
 
 public enum Status {
-	BAIXA(0, "BAIXA"),MEDIO(1, "MEDIO"),ENCERRADO(2, "ALTA");
-	
+	BAIXA(0, "BAIXA"), MEDIO(1, "MEDIO"), ENCERRADO(2, "ALTA");
+
 	private Integer codigo;
 	private String descricao;
-	
+
+	Status(int i, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public Integer getCodigo() {
 		return codigo;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
-	
+
 	public static Status toEnum(Integer cod) {
-		if(cod == null) {
+		if (cod == null) {
 			return null;
 		}
-		for(Status x : Status.values()) {
-			if(cod.equals(x.getCodigo())) {
+		for (Status x : Status.values()) {
+			if (cod.equals(x.getCodigo())) {
 				return x;
 			}
 		}
-		throw new IllegalArgumentException("Prioridade Inválido !!"); 
+		throw new IllegalArgumentException("Prioridade Inválido !!");
 	}
-	
-	
+
 }
