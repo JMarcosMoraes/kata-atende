a2c0499b90fb56d3490024ec293c1e9891020d76
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-03-19 21:59:55 -0300

    Anotações JPA para classes de dominio

--- src/main/java/com/kata/helpDesk/domain/Cliente.java
+++ src/main/java/com/kata/helpDesk/domain/Cliente.java
@@ -3,16 +3,27 @@ package com.kata.helpDesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa{
-	
-	private List<Chamado> chamados = new ArrayList<>();
+import com.kata.helpDesk.domain.enums.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Cliente extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
 
 	public Cliente() {
 		super();
+		addPerfi(Perfil.CLIENTE);
 	}
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfi(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
@@ -22,6 +33,5 @@ public class Cliente extends Pessoa{
 	public void setChamados(List<Chamado> chamados) {
 		this.chamados = chamados;
 	}
-	
-	
+
 }
--- src/main/java/com/kata/helpDesk/domain/Tecnico.java
+++ src/main/java/com/kata/helpDesk/domain/Tecnico.java
@@ -3,16 +3,28 @@ package com.kata.helpDesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.kata.helpDesk.domain.enums.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa{
 	
+
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfi(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfi(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
