80aae262e3208a5fb4d767bfaf3735c6a20efcf6
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-03-24 10:53:53 -0300

    endPoint create para tecnico

--- src/main/java/com/kata/helpDesk/domain/Tecnico.java
+++ src/main/java/com/kata/helpDesk/domain/Tecnico.java
@@ -2,8 +2,10 @@ package com.kata.helpDesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.kata.helpDesk.domain.dtos.TecnicoDTO;
 import com.kata.helpDesk.domain.enums.Perfil;
 
 import jakarta.persistence.Entity;
@@ -27,6 +29,17 @@ public class Tecnico extends Pessoa{
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
