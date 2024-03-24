1aa9901a6b909bc76e3e2e32dc78b1412bf18d5a
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-03-24 16:55:39 -0300

    CRUD Cliente

--- src/main/java/com/kata/helpDesk/domain/Cliente.java
+++ src/main/java/com/kata/helpDesk/domain/Cliente.java
@@ -2,8 +2,10 @@ package com.kata.helpDesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.kata.helpDesk.domain.dtos.ClienteDTO;
 import com.kata.helpDesk.domain.enums.Perfil;
 
 import jakarta.persistence.Entity;
@@ -25,6 +27,18 @@ public class Cliente extends Pessoa {
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 		addPerfil(Perfil.CLIENTE);
 	}
 
