9aeb18ddf8ee46f53e0e78257eb5e0fc76740ad8
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-03-24 10:05:56 -0300

    Exceção para objeto não encontrado

---
+++ src/main/java/com/kata/helpDesk/services/exceptions/ObjectnotFoundException.java
@@ -0,0 +1,14 @@
+package com.kata.helpDesk.services.exceptions;
+
+public class ObjectnotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectnotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ObjectnotFoundException(String message) {
+		super(message);
+	}
+
+}
