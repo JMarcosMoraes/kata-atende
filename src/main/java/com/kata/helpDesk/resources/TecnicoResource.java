418a470d24865dcb5996167e13c8641a0f4eda04
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-03-24 16:21:14 -0300

    endpoit delete tecnico

--- src/main/java/com/kata/helpDesk/resources/TecnicoResource.java
+++ src/main/java/com/kata/helpDesk/resources/TecnicoResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,5 +59,9 @@ public class TecnicoResource {
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
-
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
