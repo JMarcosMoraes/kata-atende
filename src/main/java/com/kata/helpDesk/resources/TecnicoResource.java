cac6f1a90492047d08f741c88e753dbd05ac3985
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-03-24 10:18:24 -0300

    endPoint findAll para tecnico

--- src/main/java/com/kata/helpDesk/resources/TecnicoResource.java
+++ src/main/java/com/kata/helpDesk/resources/TecnicoResource.java
@@ -1,6 +1,10 @@
 package com.kata.helpDesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.Repository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kata.helpDesk.domain.Tecnico;
+import com.kata.helpDesk.domain.dtos.TecnicoDTO;
 import com.kata.helpDesk.services.TecnicoService;
 
 @RestController
@@ -21,6 +26,15 @@ public class TecnicoResource {
 	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDto = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return (ResponseEntity.ok().body(listDto));
+		
 	}
 
 	
